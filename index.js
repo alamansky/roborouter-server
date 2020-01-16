536d111f2fbc0fac3e8107724503f09b44738a30
Andrew Lamansky <andrewlamansky@gmail.com>
2020-01-16 15:57:23 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+
+const PORT = 3000;
+
+app.use(express.json());
+
+app.post("/", (req, res) => {
+  let routeObj = req.body;
+
+  for (tech in routeObj) {
+    app.get(`/${tech}`, (req, res) => {
+      console.log(routeObj[tech]);
+      res.json(routeObj[tech]);
+    });
+  }
+
+  res.send("hello world");
+});
+
+app.listen(PORT, () => console.log(`server running on ${PORT}`));
