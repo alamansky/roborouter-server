8debcf3d4df2b645e329c9eb2a993d2fb7c7bc24
Andrew Lamansky <andrewlamansky@gmail.com>
2020-08-30 11:24:43 -0400

    use path module for requiring routes

--- index.js
+++ index.js
@@ -1,30 +1,31 @@
-require("dotenv").config();
-const express = require("express");
-const config = require("./config");
+/** @format */
+
+require('dotenv').config();
+const express = require('express');
+const config = require('./config');
+const path = require('path');
 
 // require routes
-const root = require("./routes/get/root");
-const upload = require("./routes/post/upload");
-const tech = require("./routes/get/tech");
+const root = require(path.join(__dirname, './routes/get/root'));
+const upload = require(path.join(__dirname, './routes/post/upload'));
+const tech = require(path.join(__dirname, './routes/get/tech'));
 
 // set up express and init db class
 const app = express();
-const db = require("./database");
+const db = require('./database');
 
 // middleware
-app.use(express.static(__dirname + "/public"));
-app.set("view engine", "pug");
+app.use(express.static(__dirname + '/public'));
+app.set('view engine', 'pug');
 app.use(express.json());
 
 // routes
-app.get("/", root);
-app.post("/upload", upload);
-app.get("/:tech", tech);
+app.get('/', root);
+app.post('/upload', upload);
+app.get('/:tech', tech);
 
 // init app
-app.listen(config.args.PORT, () =>
-  console.log(`server running on ${config.args.PORT}`)
-);
+app.listen(config.args.PORT, () => console.log(`server running on ${config.args.PORT}`));
 
 /* (async () => await populateTestData(PORT))();
  */
